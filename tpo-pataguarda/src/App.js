c8265d6168ed678c46b2ea16a308e47ae7b21a3d
araceli-cejas <128641042+araceli-cejas@users.noreply.github.com>
2024-06-18 02:29:33 -0300

    Update App.js

--- tpo-pataguarda/src/App.js
+++ tpo-pataguarda/src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Servicios from './pages/servicios';
 
 function App() {
   return (
-    <div>
-      <Header />
-   
-      <MainContent />     
-      
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<MainContent />} />
+          <Route path="/pages/servicios" element={<Servicios />} />
+          
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
